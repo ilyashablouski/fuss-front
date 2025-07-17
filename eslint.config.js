a425e15c2be7ce9a47d43609e54b831220bc03b1
i.shablovskiy <ilya.shablovskiy@gmail.com>
2025-07-17 12:06:22 +0300

    resolve import aliases

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginImportTypescript from 'eslint-plugin-import-typescript';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default [
@@ -13,6 +14,18 @@ export default [
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
+    },
+  },
+
+  {
+    // @see https://www.npmjs.com/package/eslint-plugin-import-typescript
+    files: ['**/*.astro'],
+    plugins: {
+      'import-typescript': eslintPluginImportTypescript,
+    },
+    rules: {
+      // Note: This still allows same folder imports to use relative paths. If we want to turn those off too, we need the rule 'no-relative-import'
+      'import-typescript/no-relative-parent-imports': ['warn', { onlyPathsImport: true }],
     },
   },
 ];
