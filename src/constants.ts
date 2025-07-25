96bc53051d149a2d48996419f4d9de385d7660df
i.shablovskiy <ilya.shablovskiy@gmail.com>
2025-07-25 19:27:33 +0300

    feat: add cookies popup

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const URLS = {
   instagram: 'https://www.instagram.com/fuss.london',
   appStore: 'https://apps.apple.com/gb/app/fuss-london-places-events/id6496601129',
+  download: 'https://fuss.onelink.me/mklR/2eowwt8d',
 } as const;
